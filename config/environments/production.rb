a8743909a2752350e04d4427542d63cd709aca46
Takayuki Inoue <ta.inoue.oval16@gmail.com>
2018-11-27 23:44:28 +0000

    created new sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -73,7 +73,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default :charset => "utf-8"
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
